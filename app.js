90724d43aa2cb9384cb52f418e27da29b9c4b40b
Anesidora II <vickbelo17@gmail.com>
2023-10-16 04:34:35 -0300

    prd: implementando tabelas Professor, Nota e Disciplina

--- app.js
+++ app.js
@@ -13,10 +13,24 @@ const student = require('./routes/routeStudent')
 // console.log('o que tem dentro do sequelize? ->', sequelize);
 // console.log('o que tem dentro do app = express()? ->', app);
 
+// Criando tabelas
 
 sequelize.sequelizeStudent.sync().then(() => {
     console.log('Tabela ESTUDANTE sincronizada');
   });
+  
+sequelize.sequelizeTeacher.sync().then(() => {
+    console.log('Tabela PROFESSOR sincronizada');
+  });
+  
+sequelize.sequelizeClass.sync().then(() => {
+    console.log('Tabela DISCIPLINA sincronizada');
+  });
+  
+sequelize.sequelizeGrade.sync().then(() => {
+    console.log('Tabela NOTA sincronizada');
+  });
+  
   app.listen(3000, () => {
     console.log('Servidor Express está rodando na porta 3000');
 });
--- database/models.js
+++ database/models.js
@@ -1,6 +1,8 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../databaseConfig');
   
+// Definição de modelos Estudante, Professor, Nota e Disciplina
+
 const sequelizeStudent = sequelize.define('Estudante', {
   name: {
     type: DataTypes.STRING,
@@ -17,4 +19,62 @@ const sequelizeStudent = sequelize.define('Estudante', {
   },
 });
 
-module.exports =  {sequelizeStudent, sequelize };
+ 
+const sequelizeTeacher = sequelize.define('Professor', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  age: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  class: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+
+const sequelizeClass = sequelize.define('Disciplina', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  workload: {
+    type: DataTypes.FLOAT    
+   
+  },
+});
+
+const sequelizeGrade = sequelize.define('Nota', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  workload: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+});
+
+// Definição das tabelas de associação para many-to-many. Necessário.
+const StudentDiscipline = sequelize.define('StudentDiscipline', {});
+const TeacherGrade = sequelize.define('TeacherGrade', {});
+
+//  Associações many-to-many entre Aluno e Disciplina, Professor e Aluno.
+sequelizeStudent.belongsToMany(sequelizeClass, { through: 'StudentDiscipline' });
+sequelizeClass.belongsToMany(sequelizeStudent, { through: 'StudentDiscipline' });
+
+sequelizeTeacher.belongsToMany(sequelizeStudent, { through: 'TeacherGrade' });
+sequelizeStudent.belongsToMany(sequelizeTeacher, { through: 'TeacherGrade' });
+
+// Associações one-to-many entre Aluno e Nota, Professor e Disciplina.
+sequelizeStudent.hasMany(sequelizeGrade);
+sequelizeGrade.belongsTo(sequelizeStudent);
+
+sequelizeTeacher.hasMany(sequelizeClass);
+sequelizeClass.belongsTo(sequelizeTeacher);
+
+module.exports =  {sequelizeStudent,sequelizeTeacher,sequelizeClass,sequelizeGrade, sequelize };
