03b8e5fd8f22406b027330dfc4eb43577aa2feb4
Anesidora II <vickbelo17@gmail.com>
2023-10-21 17:40:34 -0300

    PRD-03: Configuração Swagger

--- routes/routerStudent.js
+++ routes/routerStudent.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { sequelizeStudent, sequelize, sequelizeCourse } = require('../database/models');
 
+
 router.get('/', async (req, res) => {
   const sqlQuery = `
     SELECT
